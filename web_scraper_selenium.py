5b2765cd7ed4ce9922093e895f1ec5c5181e6ef2
Shahar Shoshany <Shahar9772@gmail.com>
2021-02-22 15:34:23 +0200

    Updated web_scraper_selenium.py

--- web_scraper_selenium.py
+++ web_scraper_selenium.py
@@ -1,26 +1,31 @@
 import argparse
 import os
 import time
-
+import sys
 import pandas as pd
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
+import string
+PRINTABLE = set(string.printable)
 
-PROPERTY_LISTING_TYPE = ('buy', 'rent', 'commercial', 'new homes', 'all')
+PROPERTY_LISTING_TYPE = ('buy', 'rent', 'commercial', 'new_homes', 'all')
 MAIN_URL = 'https://www.onmap.co.il/en'
 # TODO implement commercial and new homes (their html is different)
 URLS = {'buy': MAIN_URL + '/homes/buy',
         'rent': MAIN_URL + '/homes/rent',
         'commercial': MAIN_URL + '/commercial/rent/',
         'new homes': MAIN_URL + '/projects/'}
+PRICE_IDX = -1
+CITY_IDX = -1
+ADDRESS_IDX = -2
 PROPERTY_TYPE_IDX = 1
 NUM_OF_ROOMS_IDX = 0
 FLOOR_IDX = 1
 SIZE_IDX = 2
 PARKING_SPACES_IDX = 3
-SCROLL_PAUSE_TIME = 2
-COLUMNS = ['Price[NIS]', 'Property_type', 'Address', 'Rooms', 'Floor', 'Area[m^2]', 'Parking_spots']
+SCROLL_PAUSE_TIME = 1
+COLUMNS = ['Price[NIS]', 'Property_type', 'City', 'Address', 'Rooms', 'Floor', 'Area[m^2]', 'Parking_spots']
 
 
 def define_parser():
@@ -42,13 +47,9 @@ def define_parser():
 
 def create_driver():
     """
-    Creates a driver that runs in Google Chrome without the user seeing an open browser window
+    Creates a driver that runs in Google Chrome.
     """
-    # silent web_driver log
-    os.environ['WDM_LOG_LEVEL'] = '0'
-    opts = webdriver.ChromeOptions()
-    opts.headless = True
-    return webdriver.Chrome(ChromeDriverManager().install(), options=opts)
+    return webdriver.Chrome(ChromeDriverManager().install())
 
 
 def scroll(driver, verbose=False):
@@ -68,6 +69,9 @@ def scroll(driver, verbose=False):
         except NoSuchElementException:
             scroll_num += 1
         else:
+            bot_ele = driver.find_element_by_xpath("//div[@class='G3BoaHW05R4rguvqgn-Oo']")
+            time.sleep(SCROLL_PAUSE_TIME)
+            driver.execute_script("arguments[0].scrollIntoView();", bot_ele)
             break
 
 
@@ -76,17 +80,22 @@ def scrap_url(driver, url, to_print=True, save=False, verbose=False):
     Scraps properties' information given the url and either prints it to the screen and/or save it to a csv file
     """
     if verbose:
-        print(f'Accessing {url}')
+        print(f'\nAccessing {url}\n')
     driver.get(url)
     if verbose:
-        print(f"Scrolling down {url}")
-    scroll(driver, verbose=verbose)
+        print(f"\nScrolling down {url}\n")
+    scroll(driver=driver, verbose=verbose)
     if verbose:
-        print(f"Scraping {url}")
-    prices = (price.text for price in driver.find_elements_by_xpath("//span[@class='cWr2cxa0k3zKePxbqpw3L']"))
+        print(f"\nScraping {url}\n")
+
+    prices = (price.text.split()[PRICE_IDX] for price in
+              driver.find_elements_by_xpath("//span[@class='cWr2cxa0k3zKePxbqpw3L']"))
     prop_types = (prop_type.text.split('\n')[PROPERTY_TYPE_IDX] for prop_type in
                   driver.find_elements_by_xpath("//div[@class='_1bluUEiq7lEDSV1yeF9mjl']"))
-    addresses = (address.text for address in driver.find_elements_by_xpath("//div[@property='address']"))
+    cities = (address.text.split('\n')[CITY_IDX] for address in
+              driver.find_elements_by_xpath("//div[@property='address']"))
+    addresses = (address.text.split('\n')[ADDRESS_IDX] for address in
+                 driver.find_elements_by_xpath("//div[@property='address']"))
     nums_rooms = (num_rooms.text.split()[NUM_OF_ROOMS_IDX] for num_rooms in
                   driver.find_elements_by_xpath("//div[@class='yHLZr2apXqwIyhsOGyagJ']"))
     floors = (floor.text.split()[FLOOR_IDX] for floor in
@@ -95,17 +104,24 @@ def scrap_url(driver, url, to_print=True, save=False, verbose=False):
              driver.find_elements_by_xpath("//div[@class='yHLZr2apXqwIyhsOGyagJ']"))
     parking_spaces = (parking.text.split()[PARKING_SPACES_IDX] if len(parking.text.split()) > PARKING_SPACES_IDX else 0
                       for parking in driver.find_elements_by_xpath("//div[@class='yHLZr2apXqwIyhsOGyagJ']"))
-    zipped = zip(prices, prop_types, addresses, nums_rooms, floors, sizes, parking_spaces)
+    zipped = zip(prices, prop_types, cities, addresses, nums_rooms, floors, sizes, parking_spaces)
+    df = pd.DataFrame(zipped, columns=COLUMNS)
+
     if to_print:
-        for price, prop_type, address, num_rooms, floor, size, parking in zipped:
-            print(f"Price: {price}, Type: {prop_type}, Address: {address}, "
-                  f"Nº Rooms: {num_rooms}, Floor: {floor}, Size {size}, Parking: {parking}")
+        for index, row in df.iterrows():
+            print(
+                f"Price: {row['Price[NIS]']}, Type: {row['Property_type']}, City: {row['City']}, Address: {row['Address']}, "
+                f"Nº Rooms: {row['Rooms']}, Floor: {row['Floor']}, Size {row['Area[m^2]']}, Parking: {row['Parking_spots']}")
+
+    if verbose:
+        num_items = len(df)
+        print(f'\nScraped total of {num_items} items\n')
 
     if save:
         filename = f"{url.split('/')[-1]}.csv"
         if verbose:
-            print(f'Saving {filename}')
-        df = pd.DataFrame(zipped, columns=COLUMNS)
+            print(f'\nSaving {filename}\n')
+            print(df)
         df.to_csv(filename)
 
 
@@ -113,27 +129,24 @@ def main():
     """
     Prints to the screen or save it to a csv file buy and/or rent real estate scraped data from the OnMap website
     """
-
     args = define_parser()
     listing_type = {
         'buy': ['buy'],
         'rent': ['rent'],
         'commercial': ['commercial'],
-        'new homes': ['new homes'],
+        'new_homes': ['new homes'],
         'all': ['buy', 'rent', 'commercial', 'new homes']
     }
 
-    if args.property_listing_type not in PROPERTY_LISTING_TYPE:
-        print(f'You should choose of one the following: {PROPERTY_LISTING_TYPE},'
-              f' but you provided {args.property_listing_type}')
-        return
-
     urls = [URLS[key] for key in listing_type[args.property_listing_type]]
+
     driver = create_driver()
+
     for url in urls:
         scrap_url(driver, url, to_print=args.print, save=args.save, verbose=args.verbose)
+        driver.close()
     driver.quit()
-    print('Done!')
+    print('\nDone!\n')
 
 
 if __name__ == '__main__':
