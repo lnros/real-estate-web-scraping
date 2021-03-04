cad8bc62f015c04da194c5195deac62305bccc3f
Leonardo Rosenberg <38786534+lnros@users.noreply.github.com>
2021-03-04 12:04:39 +0200

    Creating classes: closes #7, resolves #2, fixes #4, fixes #3 (#18)
    * pull request for oop approach
    
    * selenium scraper in oop: todo json approach in the same main
    
    * oop ready for both versions: further tests maybe needed
    
    * forgot to commit before the single main version
    
    * forgot to commit before the single main version
    
    * changed define_parser() in config.py, make one arg for the type of the scraper(selenium or soup)
    
    * updated the value of NUM_ROOMS_XPATH in config.py
    
    * Updated soup_scraper
    Resets self._urls to empty list in the begining of _get_region_url_list
    Resets self._regional_data to empty list in the begining of _get_data_from_region
    
    * added a 'url server' print(if verbose) to _transform_to_df in soup_scraper.py
    
    Co-authored-by: Shahar Shoshany <Shahar9772@gmail.com>

---
+++ web_scraper.py
@@ -0,0 +1,34 @@
+from config import Configuration as Cfg
+from selenium_scraper import SeleniumScraper
+from soup_scraper import SoupScraper
+from utils import print_when_program_finishes
+
+
+def main():
+    Cfg.define_parser()
+    urls = [Cfg.URLS[key] for key in Cfg.LISTING_MAP[Cfg.args.property_listing_type]]
+    # if both soup and selenium are chosen, we use soup
+    if Cfg.args.scraper_type == 'soup':
+        params = {"limit": Cfg.args.limit,
+                  "to_print": Cfg.args.print,
+                  "save": Cfg.args.save,
+                  "verbose": Cfg.args.verbose}
+        scraper = SoupScraper()
+        for listing_type in Cfg.LISTING_MAP[Cfg.args.property_listing_type]:
+            scraper.scrap(listing_type, **params)
+
+    elif Cfg.args.scraper_type == 'selenium':
+        params = {"to_print": Cfg.args.print,
+                  "save": Cfg.args.save,
+                  "verbose": Cfg.args.verbose}
+        scraper = SeleniumScraper()
+        for url in urls:
+            scraper.scrap_url(url, **params)
+            scraper.driver.close()
+        scraper.driver.quit()
+
+    print_when_program_finishes()
+
+
+if __name__ == '__main__':
+    main()
