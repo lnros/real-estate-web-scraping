b87312ccd2f0bba1de562adee8af263927658cb5
Leonardo Rosenberg <38786534+lnros@users.noreply.github.com>
2021-03-26 15:27:50 +0300

    logger fixed and integration complete (#44)

--- config.py
+++ config.py
@@ -3,6 +3,36 @@ import logging
 import string
 import sys
 
+COMMIT_TO_DB_SUCCESSFUL = "_save_to_data_base: Commit to db successful"
+
+DB_SUCCESSFUL = "f_save_to_data_base: Connection to the db successful"
+
+ERROR_CONNECTION = f"failed to connect to the server."
+
+QUITTING_DRIVER = "main: Quitting driver"
+
+CLOSING_DRIVER = "main: Closing driver"
+
+SCRAPER_OBJECT = "main: Creating the scraper object"
+
+NO_URLS_FOUND_TO_SCRAPE = 'main: No urls found to scrape.'
+
+PARSER_WAS_SUCCESSFUL = 'main: CLI parser was successful.'
+
+FINISHED_SUCCESSFULLY_FETCH = "fetch_more_attributes: finished successfully"
+
+GEOFETCHER_INITIALIZED = "fetch_more_attributes: geofetcher initialized"
+
+MORE_ATTRIBUTES_STARTING = "fetch_more_attributes: starting"
+
+NEW_HOMES_FINISHED = "_scroll_new_homes: finished"
+
+SCROLL_FINISHED = "_scroll: finished"
+
+HOMES_FINISHED_SCROLLING = "_scroll_new_homes: finished scrolling"
+
+FINISHED_SCROLLING = "_scroll: finished scrolling"
+
 
 class GeoFetcherConfig:
     SEPARATOR = ", "
@@ -32,6 +62,26 @@ class DBConfig:
     USER = "uydbyi6qdkmbhd4q"
     PASSWORD = "GXB67Y5tnWYyewKEZ0OW"
     DATABASE = "brbeky1hybvf32t4ufxz"
+    INSERT_CITY_QUERY = "INSERT IGNORE INTO cities(city_name) values (%s)"
+    INSERT_LISTINGS_QUERY = "INSERT IGNORE INTO listings(listing_type) values (%s)"
+    INSERT_PROPERTY_TYPES_QUERY = "INSERT IGNORE INTO property_types(property_type) values (%s)"
+
+    FK_IDS_LIST = ['listing_id', 'property_type_id', 'city_id']
+    PRICE_COLUMN_IDX = 3
+    LATITUDE_COLUMN_IDX = -5
+
+    GET_LISTING_TYPE_ID_QUERY = "SELECT id FROM listings WHERE listing_type = %s"
+    GET_PROPERTY_TYPE_ID_QUERY = "SELECT id FROM property_types WHERE property_type = %s"
+    GET_CITY_ID_QUERY = "SELECT id FROM cities WHERE city_name = %s"
+
+    TUPLE_FIRST_ELEMENT_IDX = 0
+
+    LISTING_TYPE_IDX = 0
+    PROPERTY_TYPE_IDX = 1
+    CITY_IDX = 2
+
+    SEPARATOR = ","
+    TABLE_FEEDER_COLUMN_IDX = 3
 
 
 class Configuration:
@@ -41,6 +91,15 @@ class Configuration:
 
     # class attr
     args = None
+
+    # PARAMETERS KWARGS KEYS
+    VERBOSE_KEY = 'verbose'
+    LIMIT_KEY = 'limit'
+    PRINT_KEY = 'to_print'
+    SAVE_KEY = 'save'
+    DB_KEY = 'to_database'
+    FETCH_KEY = 'fetch_info'
+    LISTING_TYPE_KEY = 'listing_type'
 
     # CONSTANTS FOR SCRAPING
     PRINTABLE = set(string.printable)
@@ -71,6 +130,7 @@ class Configuration:
     INVALID_FLOOR_TEXT_SIZE = 1
     NOT_SELENIUM_PRINTING_HASH_CONSTANT = 20
     NONE = 'none'
+    DICT_PROPERTY_ID = {'id': 'propertiesList'}
     # INDICES FOR PARSING
     NOT_SELENIUM_PARSING_FILE_IDX = 0
     ELEM_TO_SCROLL_IDX = -1
@@ -88,11 +148,30 @@ class Configuration:
     URL_SPLIT_SEPARATOR = '/'
     NOT_SELENIUM_SEPARATOR = '.'
     SEPARATOR = ", "
+    PROPERTIES_LIST_IDX = 1
+    LEN_PROPER = 2
 
     # XPATHS AND SELENIUM COMMANDS
     SCROLL_COMMAND = "arguments[0].scrollIntoView();"
     PROPERTIES_XPATH = "//div[@style='position: relative;']"
     BOTTOM_PAGE_XPATH = "//div[@class='G3BoaHW05R4rguvqgn-Oo']"
+
+    # Handling strings
+    ENCODING = "ISO-8859-8"
+    COMMERCIAL_FILENAME = "commercial.csv"
+    NEW_HOMES_FILENAME = "new_homes.csv"
+    PROJECT = 'project'
+    COMMERCIAL = 'commercial'
+
+    # DF columns names
+    PRICE_COL = 'Price'
+    ROOM_COL = 'Rooms'
+    FLOOR_COL = 'Floor'
+    AREA_COL = 'Area'
+    CITY_COL = 'City'
+    PARKING_COL = 'Parking_spots'
+    PROP_TYPE_COL = 'Property_type'
+    LIST_TYPE_COL = 'listing_type'
 
     @classmethod
     def define_parser(cls):
@@ -126,7 +205,25 @@ class Configuration:
 
 
 class Logger:
+    """
+    This class handles logging for the entire web scraping process
+    """
     logger = None
+    scroll_finished = FINISHED_SCROLLING
+    scroll_finished_new_home = HOMES_FINISHED_SCROLLING
+    end_scroll_function = SCROLL_FINISHED
+    end_scroll_new_home = NEW_HOMES_FINISHED
+    fetch_more_init = MORE_ATTRIBUTES_STARTING
+    geofetcher_init = GEOFETCHER_INITIALIZED
+    end_fetch_more_att = FINISHED_SUCCESSFULLY_FETCH
+    main_cli = PARSER_WAS_SUCCESSFUL
+    main_no_url = NO_URLS_FOUND_TO_SCRAPE
+    main_scrape_obj = SCRAPER_OBJECT
+    main_closing_driver = CLOSING_DRIVER
+    main_quit_drive = QUITTING_DRIVER
+    error_connect_server = ERROR_CONNECTION
+    connection_successful = DB_SUCCESSFUL
+    commit_successful = COMMIT_TO_DB_SUCCESSFUL
 
     @classmethod
     def start_logging(cls):
@@ -145,3 +242,323 @@ class Logger:
         stream_handler.setLevel(logging.ERROR)
         stream_handler.setFormatter(formatter)
         cls.logger.addHandler(stream_handler)
+
+    @staticmethod
+    def scroll_error(ele_to_scroll):
+        """
+        Message for the logger in case of error when scrolling.
+        ----
+        :param ele_to_scroll: html element to look for when scrolling
+        :type ele_to_scroll: str
+        :return: error message
+        :rtype: str
+        """
+        return f"_scroll: ele_to_scroll should have a content but it is {ele_to_scroll}"
+
+    @staticmethod
+    def scroll_new_homes(prev_len):
+        """
+        Message for the logger in when scrolling.
+        ----
+        :param prev_len: number of elements found when scrolling
+        :type prev_len: int
+        :return: message
+        :rtype: str
+        """
+        return f"_scroll_new_homes:prev_len {prev_len}"
+
+    @staticmethod
+    def end_save_csv(url):
+        """
+        Message for the logger when finished saving an url content to a csv
+        ----
+        :param url: url address
+        :type url: int
+        :return: message
+        :rtype: str
+        """
+        return f"_save_to_csv: finished {url}"
+
+    @staticmethod
+    def init_print_save_df(url, to_print, save, to_database, verbose, listing_type):
+        """
+        Message for the logger at beginning of print_save_df function
+        ----
+        :param url: url address
+        :type url: str
+        :param listing_type: type of listing: buy, rent, commercial, new_home
+        :type listing_type: str
+        :param to_print: if true, it prints the dataframe to the screen
+        :type to_print: bool
+        :param save: if true, it saves the dataframe into a csv file
+        :type save: bool
+        :param to_database: if true, it saves the new information from the dataframe to the database
+        :type to_database: bool
+        :param verbose: if true, it prints relevant information to the user
+        :type verbose: bool
+        """
+        return f"_print_save_df: Checking if print {url}, to_print={to_print}, save={save}, to_database={to_database}, " \
+               f"verbose={verbose}, listing_type={listing_type}"
+
+    @staticmethod
+    def saving_print_save_df(url, to_print, save, to_database, verbose, listing_type):
+        """
+        Message for the logger before saving to a csv in print_save_df function
+        ----
+        :param url: url address
+        :type url: str
+        :param listing_type: type of listing: buy, rent, commercial, new_home
+        :type listing_type: str
+        :param to_print: if true, it prints the dataframe to the screen
+        :type to_print: bool
+        :param save: if true, it saves the dataframe into a csv file
+        :type save: bool
+        :param to_database: if true, it saves the new information from the dataframe to the database
+        :type to_database: bool
+        :param verbose: if true, it prints relevant information to the user
+        :type verbose: bool
+        """
+        return f"_print_save_df: Saving into csv {url}, to_print={to_print}, save={save}, to_database={to_database}, " \
+               f"verbose={verbose}, listing_type={listing_type}"
+
+    @staticmethod
+    def db_print_save_df(url, to_print, save, to_database, verbose, listing_type):
+        """
+        Message for the logger before saving into the db in print_save_df function
+        ----
+        :param url: url address
+        :type url: str
+        :param listing_type: type of listing: buy, rent, commercial, new_home
+        :type listing_type: str
+        :param to_print: if true, it prints the dataframe to the screen
+        :type to_print: bool
+        :param save: if true, it saves the dataframe into a csv file
+        :type save: bool
+        :param to_database: if true, it saves the new information from the dataframe to the database
+        :type to_database: bool
+        :param verbose: if true, it prints relevant information to the user
+        :type verbose: bool
+        """
+        return f"_print_save_df: Saving into db {url}, to_print={to_print}, save={save}, to_database={to_database}, " \
+               f"verbose={verbose}, listing_type={listing_type}"
+
+    @staticmethod
+    def end_print_save(url):
+        """
+        Message for the logger when finished running the function _print_save_df
+        ----
+        :param url: url address
+        :type url: int
+        :return: message
+        :rtype: str
+        """
+        return f"_print_to_save: finished {url}"
+
+    @staticmethod
+    def pulling_row_info(row_number):
+        """
+        Message for the logger when pulling row information in fetch_more_attributes function
+        ----
+        :param row_number: row number in the dataframe
+        :type row_number: int
+        :return: message
+        :rtype: str
+        """
+        return f"fetch_more_attributes: Pulling info for row {row_number}"
+
+    @staticmethod
+    def exception_fetch_more_attributes(row_number, exception):
+        """
+        Message for the logger when an exception occurred when pulling row information in fetch_more_attributes function
+        ----
+        :param row_number: row number in the dataframe
+        :type row_number: int
+        :param exception: error message
+        :type exception: exception
+        :return: message
+        :rtype: str
+        """
+        return f"fetch_more_attributes: row {row_number}, {exception}"
+
+    @staticmethod
+    def not_fetched(fetch_info):
+        """
+        Message for the logger when additional information was not fetched
+        ----
+        :param fetch_info: row number in the dataframe
+        :type fetch_info: bool
+        :return: message
+        :rtype: str
+        """
+        return f"fetch_more_attributes: fetch info == {fetch_info}"
+
+    @staticmethod
+    def creating_df(url):
+        """
+        Message for the logger when _create_df is called
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"create_df: Creating dataframe from {url}"
+
+    @staticmethod
+    def created_df(url):
+        """
+        Message for the logger when _create_df is finished
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"create_df: Created dataframe from {url} successfully"
+
+    @staticmethod
+    def scraping(url):
+        """
+        Message for the logger when scrap_url is called
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"scrap_url: Scrolling {url}"
+
+    @staticmethod
+    def before_scroll(url):
+        """
+        Message for the logger before _scroll is called
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"scrap_url: Scrolling {url} - not new_homes"
+
+    @staticmethod
+    def before_scroll_new_home(url):
+        """
+        Message for the logger before _scroll_new_homes is called
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"scrap_url: Scrolling {url} - new_homes"
+
+    @staticmethod
+    def before_scraping(url):
+        """
+        Message for the logger before starting to actually scrape in scrap_url
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"scrap_url: Scraping {url}"
+
+    @staticmethod
+    def finished_scraping(url):
+        """
+        Message for the logger at the end of scrap_url
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"scrap_url: finished {url}"
+
+    @staticmethod
+    def main_scraping(url):
+        """
+        Message for the logger before calling scrap_url
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"main: Scraping {url}"
+
+    @staticmethod
+    def main_scraped_success(url):
+        """
+        Message for the logger after all scraping operations are done for the particular url
+        ----
+        :param url: url address
+        :type url: str
+        :return: message
+        :rtype: str
+        """
+        return f"main: Scrapped {url} successfully"
+
+    @staticmethod
+    def connect_to_server(listing, verbose):
+        """
+        Message for the logger before connecting to db server
+        ----
+        :param listing: listing type of the dataframe
+        :type listing: str
+        :param verbose: whether nor not to print relevant info to the user
+        :type verbose: bool
+        :return: message
+        :rtype: str
+        """
+        return f"_save_to_data_base: Connecting to the db listing_type={listing}, verbose={verbose}"
+
+    @staticmethod
+    def insert_city_error(city):
+        """
+        Message for the logger when error for inserting existing city in cities table
+        ----
+        :param city: city already in table
+        :type city: str
+        :return: message
+        :rtype: str
+        """
+        return f"_save_to_data_base: {city} is already in cities."
+
+    @staticmethod
+    def insert_city_error(listing):
+        """
+        Message for the logger when error for inserting existing listing type in listings table
+        ----
+        :param listing: listing already in table
+        :type listing: str
+        :return: message
+        :rtype: str
+        """
+        return f"_save_to_data_base: {listing} is already in listings."
+
+    @staticmethod
+    def insert_city_error(property):
+        """
+        Message for the logger when error for inserting existing property in properties table
+        ----
+        :param property: property already in table
+        :type property: str
+        :return: message
+        :rtype: str
+        """
+        return f"_save_to_data_base: {property} is already in properties."
+
+    @staticmethod
+    def insert_row_error(row):
+        """
+        Message for the logger when error for inserting existing property in properties table
+        ----
+        :param row: row already in table
+        :type row: pd.Series
+        :return: message
+        :rtype: str
+        """
+        return f"_save_to_data_base: {row} is already in properties. "
+
--- web_scraper.py
+++ web_scraper.py
@@ -6,30 +6,40 @@ from utils import print_when_program_finishes
 
 
 def main():
+    """
+    This program scrapes real estate information from 'https://www.onmap.co.il/en' and it can:
+    - Print it to the user
+    - Save it to csv
+    - Save into a database
+    A Google Chrome webpage will open and close for every different url it scrapes within the website.
+    There are at most 4 different pages for it to scrape: buy, rent, commercial, new homes
+    If the user chooses to fetch more information, beware that this is a lengthy process that
+    takes around one second per property scraped.
+    """
     Log.start_logging()
     Cfg.define_parser()
-    Log.logger.info('main: CLI parser was successful.')
+    Log.logger.info(Log.main_cli)
     urls = [Cfg.URLS[key] for key in Cfg.LISTING_MAP[Cfg.args.property_listing_type]]
     if len(urls) == 0:
-        Log.logger.error('main: No urls found to scrape.')
+        Log.logger.error(Log.main_no_url)
     for i, url in enumerate(urls):
-        params = {"limit": Cfg.args.limit,
-                  "to_print": Cfg.args.print,
-                  "save": Cfg.args.save,
-                  "verbose": Cfg.args.verbose,
-                  "to_database": Cfg.args.database,
-                  "fetch_info": Cfg.args.fetch,
-                  "listing_type": Cfg.LISTING_MAP[Cfg.args.property_listing_type][i]}
+        params = {Cfg.LIMIT_KEY: Cfg.args.limit,
+                  Cfg.PRINT_KEY: Cfg.args.print,
+                  Cfg.SAVE_KEY: Cfg.args.save,
+                  Cfg.VERBOSE_KEY: Cfg.args.verbose,
+                  Cfg.DB_KEY: Cfg.args.database,
+                  Cfg.FETCH_KEY: Cfg.args.fetch,
+                  Cfg.LISTING_TYPE_KEY: Cfg.LISTING_MAP[Cfg.args.property_listing_type][i]}
         time.sleep(Cfg.BETWEEN_URL_PAUSE)
-        Log.logger.debug("main: Creating the scraper object")
+        Log.logger.debug(Log.main_scrape_obj)
         scraper = SeleniumScraper()
-        Log.logger.debug(f"main: Scraping {url}")
+        Log.logger.debug(Log.main_scraping(url))
         scraper.scrap_url(url, **params)
-        Log.logger.debug("main: Closing driver")
+        Log.logger.debug(Log.main_closing_driver)
         scraper.driver.close()
-        Log.logger.debug("main: Quitting driver")
+        Log.logger.debug(Log.main_quit_drive)
         scraper.driver.quit()
-        Log.logger.info(f"main: Scrapped {url} successfully")
+        Log.logger.info(Log.main_scraped_success(url))
 
     print_when_program_finishes()
 
