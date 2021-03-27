11b8a2cbe4e3e07cffbfe421ea91643dbcbafbfc
Leonardo Rosenberg <nudel.rosenberg@gmail.com>
2021-03-27 16:19:26 +0300

    solving conflict for PR

--- data_base_feeder.py
+++ data_base_feeder.py
@@ -1,136 +1,142 @@
-import sys
 import numpy as np
 import pymysql
+import sys
 from config import DBConfig
-from config import Configuration as Cfg
 from config import Logger as Log
+from config import Configuration as Cfg
 from utils import *
 
-INSERT_CITY_QUERY = "INSERT IGNORE INTO cities(city_name) values (%s)"
-INSERT_LISTINGS_QUERY = "INSERT IGNORE INTO listings(listing_type) values (%s)"
-INSERT_PROPERTY_TYPES_QUERY = "INSERT IGNORE INTO property_types(property_type) values (%s)"
 
-FK_IDS_LIST = ['listing_id', 'property_type_id', 'city_id']
-PRICE_COLUMN_IDX = 3
-LATITUDE_COLUMN_IDX = -5
-
-GET_LISTING_TYPE_ID_QUERY = "SELECT id FROM listings WHERE listing_type = %s"
-GET_PROPERTY_TYPE_ID_QUERY = "SELECT id FROM property_types WHERE property_type = %s"
-GET_CITY_ID_QUERY = "SELECT id FROM cities WHERE city_name = %s"
-
-TUPLE_FIRST_ELEMENT_IDX = 0
-
-LISTING_TYPE_IDX = 0
-PROPERTY_TYPE_IDX = 1
-CITY_IDX = 2
-
-
-class DataBaeFeeder:
-
+class DataBaseFeeder:
+    """
+    This class handles the database that stores the information scraped from the website "On Map"
+    """
     def __init__(self, df, listing_type, verbose=False):
+        """
+        :param df: dataframe to have its information saved into the database
+        :type df: pd.DataFrame
+        :param verbose: if true, it prints relevant information to the user
+        :type verbose: bool
+        :param verbose: if true, it prints relevant information to the user
+        :type verbose: bool
+        """
         self.df = df.replace(np.nan, None)
         self.listing_type = listing_type
         self.verbose = verbose
 
     def _connect_to_server(self):
         """
-        connecting to the data base host server.
+        Connecting to the data base host server.
         """
         print_database(verbose=self.verbose)
-        Log.logger.debug(f"_connect_to_server: Connecting to the db"
-                         f" listing_type={self.listing_type}, verbose={self.verbose}")
+        Log.logger.debug(Log.connect_to_server(self.listing_type, self.verbose))
         try:
             self.connection = pymysql.connect(DBConfig.HOST, DBConfig.USER, DBConfig.PASSWORD, DBConfig.DATABASE)
             self.cursor = self.connection.cursor()
             self.connection.commit()
         except pymysql.err.OperationalError:
-            Log.logger.error(f"_connect_to_server: failed to connect to the server: {DBConfig.HOST}. ")
+            Log.logger.error(Log.error_connect_server)
             sys.exit(1)
         else:
-            Log.logger.info("f_connect_to_server: Connection to the db successful")
+            Log.logger.info(Log.connection_successful)
 
     def _city_table_feeder(self):
         """
-        adding new unique cities into the cities table.
+        Adding new unique cities into the cities table.
         """
-        for city in self.df['City'].unique():
+        for city in self.df[Cfg.CITY_COL].unique():
             try:
-                self.cursor.execute(INSERT_CITY_QUERY, city)
+                self.cursor.execute(DBConfig.INSERT_CITY_QUERY, city)
                 self.connection.commit()
             except pymysql.err.IntegrityError:
-                Log.logger.error(f"_city_table_feeder: {city} is already in cities. ")
+                Log.logger.error(Log.insert_city_error(city))
 
     def _listing_type_table_feeder(self):
         """
-        adding new unique listings into the listings table.
+        Adding new unique listings into the listings table.
         """
-        for listing in self.df['listing_type'].unique():
+        for listing in self.df[Cfg.LIST_TYPE_COL].unique():
             try:
-                self.cursor.execute(INSERT_LISTINGS_QUERY, listing)
+                self.cursor.execute(DBConfig.INSERT_LISTINGS_QUERY, listing)
                 self.connection.commit()
             except pymysql.err.IntegrityError:
-                Log.logger.error(f"_listing_type_table_feeder: {listing} is already in listings. ")
+                Log.logger.error(Log.insert_city_error(listing))
 
     def _property_type_table_feeder(self):
         """
-        adding new unique property types into the property_types table.
+        Adding new unique property types into the property_types table.
         """
-        for prop in self.df['Property_type'].unique():
+        for prop in self.df[Cfg.PROP_TYPE_COL].unique():
             try:
-                self.cursor.execute(INSERT_PROPERTY_TYPES_QUERY, prop)
+                self.cursor.execute(DBConfig.INSERT_PROPERTY_TYPES_QUERY, prop)
                 self.connection.commit()
             except pymysql.err.IntegrityError:
-                Log.logger.error(f"_property_type_table_feeder: {prop} is already in property_types. ")
+                Log.logger.error(Log.insert_city_error(prop))
 
     def _get_columns_list(self):
         """
-        returns relevant columns, in both list and string formats, to feed the properties table.
+        :return: returns relevant columns, in both list and string formats, to feed the properties table.
+        :rtype: tuple
         """
         columns = [str(i) for i in self.df.columns.tolist()]
-        columns_list = FK_IDS_LIST + columns[PRICE_COLUMN_IDX:]
-        columns_list_string = Cfg.SEPARATOR.join(columns_list)
+        columns_list = DBConfig.FK_IDS_LIST + columns[DBConfig.PRICE_COLUMN_IDX:]
+        columns_list_string = DBConfig.SEPARATOR.join(columns_list)
         return columns_list, columns_list_string
 
     def _get_ids_values(self, row):
         """
-        returns id's values of cities, listings and property_types in order to fee the properties table.
+        :return: returns id's values of cities, listings and property_types in order to fee the properties table.
+        :rtype: tuple
         """
-        self.cursor.execute(GET_LISTING_TYPE_ID_QUERY, tuple(row)[LISTING_TYPE_IDX])
-        listings_id = self.cursor.fetchone()[TUPLE_FIRST_ELEMENT_IDX]
+        self.cursor.execute(DBConfig.GET_LISTING_TYPE_ID_QUERY, tuple(row)[DBConfig.LISTING_TYPE_IDX])
+        listings_id = self.cursor.fetchone()[DBConfig.TUPLE_FIRST_ELEMENT_IDX]
 
-        self.cursor.execute(GET_PROPERTY_TYPE_ID_QUERY, tuple(row)[PROPERTY_TYPE_IDX])
-        property_type_id = self.cursor.fetchone()[TUPLE_FIRST_ELEMENT_IDX]
+        self.cursor.execute(DBConfig.GET_PROPERTY_TYPE_ID_QUERY, tuple(row)[DBConfig.PROPERTY_TYPE_IDX])
+        property_type_id = self.cursor.fetchone()[DBConfig.TUPLE_FIRST_ELEMENT_IDX]
 
-        self.cursor.execute(GET_CITY_ID_QUERY, tuple(row)[CITY_IDX])
-        city_id = self.cursor.fetchone()[TUPLE_FIRST_ELEMENT_IDX]
+        self.cursor.execute(DBConfig.GET_CITY_ID_QUERY, tuple(row)[DBConfig.CITY_IDX])
+        city_id = self.cursor.fetchone()[DBConfig.TUPLE_FIRST_ELEMENT_IDX]
 
         return listings_id, property_type_id, city_id
 
+    @staticmethod
+    def _insert_property_query(columns_list_string, columns_list):
+        """
+        Inserts a new property into the properties table
+        ----
+        :param columns_list_string: a string with the names of the database columns
+        :type columns_list_string: str
+        :param columns_list: a list with the names of the database columns
+        :type columns_list: list
+        """
+        return "INSERT IGNORE INTO properties" \
+                  " (" + columns_list_string + ") VALUES (" + "%s, " * (len(columns_list) - 1) + "%s)"
+
     def _properties_table_feeder(self):
         """
-        adding new records into the properties table.
+        Adding new records into the properties table.
         """
         for i, row in self.df.iterrows():
             listings_id, property_type_id, city_id = self._get_ids_values(row)
             columns_list, columns_list_string = self._get_columns_list()
             values = tuple([listings_id, property_type_id, city_id] +
-                           row.values[PRICE_COLUMN_IDX:].tolist())
+                           row.values[DBConfig.PRICE_COLUMN_IDX:].tolist())
 
-            sql = "INSERT IGNORE INTO properties" \
-                  " (" + columns_list_string + ") VALUES (" + "%s, " * (len(columns_list) - 1) + "%s)"
+            sql = self._insert_property_query(columns_list_string, columns_list)
 
             try:
-                values = tuple([listings_id, property_type_id, city_id] + row.values[PRICE_COLUMN_IDX:].tolist())
+                values = tuple(
+                    [listings_id, property_type_id, city_id] + row.values[DBConfig.TABLE_FEEDER_COLUMN_IDX:].tolist())
                 self.cursor.execute(sql, values)
                 self.connection.commit()
             except pymysql.err.IntegrityError:
-                Log.logger.error(f"_properties_table_feeder: {row} is already in properties. ")
+                Log.logger.error(Log.insert_row_error(row))
             else:
-                Log.logger.info("_properties_table_feeder: Commit to db successful")
+                Log.logger.info(Log.commit_successful)
 
     def _data_base_feeder(self):
         """
-        feeding the data base with the scraped data frame.
+        Feeding the data base with the scraped data frame.
         First feeding cities, listings and property_types tables.
         Second, feeding the properties table which have foreign keys constraints
         with cities, listings and property_types tables.
@@ -143,6 +149,6 @@ class DataBaeFeeder:
 
     def update_db(self):
         """
-        updates the data base with the scraped data frame.
+        Updates the data base with the scraped data frame.
         """
         self._data_base_feeder()
